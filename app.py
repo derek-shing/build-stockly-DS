2d2edc43c4aa93e2b9d5849f2d34af8d61a77aa3
derek-shing <hpshing@gmail.com>
2019-05-20 12:31:06 -0700

    start webserver

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask , request
 import pandas as pd
 import numpy as np
+import json
 
 import os
 
@@ -14,9 +15,10 @@ APP = Flask(__name__)
 
 @APP.route('/')
 def hello_world():
-
-    response='HelloWorld'
-    #print(response)
+    dict1 = {'TA': {'sell':0.5,'hold':0.25,'buy':0.25}, 'Sentiment':{'sell':0.5,'hold':0.25,'buy':0.25}}
+    json1 = json.dumps(dict1)
+    response=json1
+    print(response)
 
 
     return response
