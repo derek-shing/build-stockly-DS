95a8357f67d05c094e2569b7f9348bb6c94bd805
derek-shing <hpshing@gmail.com>
2019-05-21 12:02:00 -0700

    basemodel

--- app.py
+++ app.py
@@ -67,14 +67,18 @@ def generate_target(df):
 @APP.route('/')
 def hello_world():
     input ="MU"
-    market_df = generate_df(input)
-    market_df = market_df.dropna()
+    #market_df = generate_df(input)
+    #market_df = market_df.dropna()
+
     X = market_df[['5. volume', 'MACD', 'AROONOSC',
                    'MACD_Hist', 'MACD_Signal', 'DX', 'SlowD', 'SlowK']]
     #print(X[0])
     sc = StandardScaler()
     X = sc.fit_transform(X)
-    y_prebro = model.predict_proba(X[0].reshape(1, -1))
+    test = array([[ 0.84330129, -0.87267448, -1.55021623, -1.14815012, -0.54096114,
+         1.74642336, -1.14298853, -1.59289229]])
+    #y_prebro = model.predict_proba(X[0].reshape(1, -1))
+    y_prebro = model.predict_proba(test)
     print(y_prebro)
     dict1 = {'TA': {'sell':y_prebro[0][0],'hold':y_prebro[0][1],'buy':y_prebro[0][2]}, 'Sentiment':{'sell':0.5,'hold':0.25,'buy':0.25}}
     json1 = json.dumps(dict1)
