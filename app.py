bbf40ed8a8c1fae232b2714c0a5d5e224fd791a2
derek-shing <hpshing@gmail.com>
2019-05-21 12:28:15 -0700

    basemodel

--- app.py
+++ app.py
@@ -66,18 +66,17 @@ def generate_target(df):
 
 @APP.route('/')
 def hello_world():
-    input ="MU"
-    #market_df = generate_df(input)
-    #market_df = market_df.dropna()
+    input ="AAPL"
+    market_df = generate_df(input)
+    market_df = market_df.dropna()
 
-    #X = market_df[['5. volume', 'MACD', 'AROONOSC',
-    #               'MACD_Hist', 'MACD_Signal', 'DX', 'SlowD', 'SlowK']]
+    X = market_df[['5. volume', 'MACD', 'AROONOSC','MACD_Hist', 'MACD_Signal', 'DX', 'SlowD', 'SlowK']]
     #print(X[0])
-    #sc = StandardScaler()
-    #X = sc.fit_transform(X)
-    test = np.array([[ 0.84330129, -0.87267448, -1.55021623, -1.14815012, -0.54096114,1.74642336, -1.14298853, -1.59289229]])
-    #y_prebro = model.predict_proba(X[0].reshape(1, -1))
-    y_prebro = model.predict_proba(test)
+    sc = StandardScaler()
+    X = sc.fit_transform(X)
+    #test = np.array([[ 0.84330129, -0.87267448, -1.55021623, -1.14815012, -0.54096114,1.74642336, -1.14298853, -1.59289229]])
+    y_prebro = model.predict_proba(X[0].reshape(1, -1))
+    #y_prebro = model.predict_proba(test)
     print(y_prebro)
     dict1 = {'TA': {'sell':y_prebro[0][0],'hold':y_prebro[0][1],'buy':y_prebro[0][2]}, 'Sentiment':{'sell':0.5,'hold':0.25,'buy':0.25}}
     #dict1 = {'TA': {'sell': 0.5, 'hold': 0.25, 'buy': 0.25},'Sentiment': {'sell': 0.5, 'hold': 0.25, 'buy': 0.25}}
