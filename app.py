4ca6ad0c5f89b506e7476e27ceaad3d85e37a4a9
derek-shing <hpshing@gmail.com>
2019-05-22 01:12:13 -0700

    5 API Key

--- app.py
+++ app.py
@@ -15,13 +15,19 @@ APP = Flask(__name__)
 model = pickle.load( open( "model.p", "rb" ) )
 
 
+
+
 def generate_df(ticker):
     macd = 'https://www.alphavantage.co/query?function=MACD&symbol=' + ticker + '&interval=daily&series_type=open&apikey=NXAA2P2XI1GQSYPG'
     response1 = requests.get(macd)
+    if "Note" in response1.json().keys():
+        return (response1.json()["Note"])
     df_macd = pd.DataFrame.from_dict(response1.json()['Technical Analysis: MACD']).T
 
     stoch = 'https://www.alphavantage.co/query?function=STOCH&symbol=' + ticker + '&interval=daily&apikey=PLHD2X5ETW6R4GF1'
     response2 = requests.get(stoch)
+    if "Note" in response2.json().keys():
+        return (response2.json()["Note"])
     df_stoch = pd.DataFrame.from_dict(response2.json()['Technical Analysis: STOCH']).T
 
     # rsi = 'https://www.alphavantage.co/query?function=RSI&symbol='+ticker+'&interval=daily&time_period=10&series_type=open&apikey=NXAA2P2XI1GQSYPG'
@@ -30,14 +36,20 @@ def generate_df(ticker):
 
     aroon = 'https://www.alphavantage.co/query?function=AROONOSC&symbol=' + ticker + '&interval=daily&time_period=10&apikey=SBYDUUQMNB6L5GXN'
     response4 = requests.get(aroon)
+    if "Note" in response4.json().keys():
+        return (response4.json()["Note"])
     df_aroon = pd.DataFrame.from_dict(response4.json()['Technical Analysis: AROONOSC']).T
 
     dx = 'https://www.alphavantage.co/query?function=DX&symbol=' + ticker + '&interval=daily&time_period=10&apikey=PKRRAV3RHXK8B9GL'
     response5 = requests.get(dx)
+    if "Note" in response5.json().keys():
+        return (response5.json()["Note"])
     df_dx = pd.DataFrame.from_dict(response5.json()['Technical Analysis: DX']).T
 
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + ticker + '&interval=5min&outputsize=full&apikey=08VAMYYFT8BQO48Q'
     response6 = requests.get(url)
+    if "Note" in response6.json().keys():
+        return (response6.json()["Note"])
     df = pd.DataFrame.from_dict(response6.json()['Time Series (Daily)']).T
 
     # Join all the dataset
@@ -74,6 +86,9 @@ def hello_world():
     if request.method == 'POST':
         input = request.values['ticker']
     market_df = generate_df(input)
+    if type(market_df)==str:
+        return market_df
+
     market_df = market_df.dropna()
 
     X = market_df[['5. volume', 'MACD', 'AROONOSC','MACD_Hist', 'MACD_Signal', 'DX', 'SlowD', 'SlowK']]
